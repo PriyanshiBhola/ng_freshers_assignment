b1e5a261ad2b914d9503ef59be4e66bfef2c0bb5
PriyanshiBhola <43310746+PriyanshiBhola@users.noreply.github.com>
2020-04-23 01:18:26 +0530

    Assignment 2 Updated

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,27 +1,28 @@
-import { Component,  ViewEncapsulation } from '@angular/core';
-import { AssignmentComponent, Todo } from '../../assignment.component';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+export interface Todo {
+  id: string;
+  type: string;
+  description: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-assignment2',
   templateUrl: './assignment2.component.html',
-  styleUrls: ['./assignment2.component.css']
+  styleUrls: ['./assignment2.component.css'],
+  //encapsulation: ViewEncapsulation.None
 })
 export class Assignment2Component {
-  list : Todo[];
-  constructor(private asgcomp : AssignmentComponent) { }
 
+  headers = ["id", "type", "description", "completed"];
 
-  createTableData(input : string) : HTMLTableDataCellElement{
-    let cell = document.createElement('td');
-    cell.textContent = input;
-    return cell;
-  }
-  ngOnInit(){
-    
-     this.list = this.asgcomp.todos;
-   
-  }
-
+  todos: Todo[] = [
+    { id: 'TODO_1', type: 'Teach TypeScript', description: 'TypeScript is an awesome tech', completed: false },
+    { id: 'TODO_2', type: 'Teach Angular', description: 'Angular is the future', completed: true },
+    { id: 'TODO_3', type: 'Teach NodeJS', description: 'Much appreciated server-side tech', completed: true },
+    { id: 'TODO_4', type: 'Teach JavaScript', description: 'A ubiquitous language in browser', completed: false }
+  ];
   addClass(event){
     event.target.parentNode.className = "active";
   }
